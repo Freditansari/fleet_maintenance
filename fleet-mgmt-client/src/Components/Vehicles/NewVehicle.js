17ef591ea177d09efd2458e1ef697eb03deaa8f0
Freditansari <Freditansari@users.noreply.github.com>
2020-10-10 02:04:09 -0400

    hooked up new vehicle form to the state. will be implementing the database after this

--- fleet-mgmt-client/src/Components/Vehicles/NewVehicle.js
+++ fleet-mgmt-client/src/Components/Vehicles/NewVehicle.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 const NewVehicle = props => {
+    const newVehicleState = {
+        vehicleName: '',
+        type: '',
+        model:'',
+        hullNumber :'', 
+        isOperational:true
+
+    }
+    const [NewVehicle, setNewVehicle] = useState(newVehicleState);
+
+    const handleInputChange = event =>{
+        const {name, value} = event.target;
+        setNewVehicle({...NewVehicle, [name]: value});
+        // console.log(NewVehicle)
+    }
+
+    const handleNewVehicleSubmit =(vehicle) =>{
+        console.log(NewVehicle)
+        // todo handle to send database
+
+    }
+
     return (
         <div class = "container">
             <form class="border border-light p-5 mt-5">
@@ -10,26 +32,30 @@ const NewVehicle = props => {
 
 
             <label for="VehicleName">Vehicle Name</label>
-            <input type="text" id="VehicleName" class="form-control mb-4" placeholder="Enter a Vehicle name" />
+            <input type="text" name ="vehicleName" value={NewVehicle.name} onChange={handleInputChange} class="form-control mb-4" placeholder="Enter a Vehicle name" />
 
-            <select class="browser-default custom-select mb-4" id="select">
-                <option value="" disabled="" selected="">Choose Vehicle Type</option>
-                <option value="1">Light Vehicle</option>
-                <option value="2">Excavators</option>
-                <option value="3">Bulldozers</option>
-                <option value="4">Tractors</option>
-                <option value="5">Trucks</option>
+            <label for="selectCategory">Select vehicle type:</label>
+            <select class="browser-default custom-select mb-4" id="select" name="type" value={NewVehicle.type} onChange={handleInputChange}>
+                {/* <option value="" disabled="" selected="">Choose Vehicle Type</option> */}
+                <option value="Light Vehicle<">Light Vehicle</option>
+                <option value="Excavators">Excavators</option>
+                <option value="Bulldozers">Bulldozers</option>
+                <option value="Tractors">Tractors</option>
+                <option value="Trucks">Trucks</option>
             </select>
 
 
             <label for="vehicle_model">Model</label>
-            <input type="text" id="vehicle_model" class="form-control mb-4" placeholder="Enter Model" />
+            <input type="text" id="vehicle_model" name="model"  value={NewVehicle.model} class="form-control mb-4" onChange={handleInputChange} placeholder="Enter Model" />
 
             <label for="hull_number_vehicle">Hull Number</label>
-            <input type="text" id="hull_number_vehicle" class="form-control mb-4" placeholder="Enter Hull Numbers" />
+            <input type="text" id="hull_number_vehicle" name="hullNumber"  value={NewVehicle.hullNumber} class="form-control mb-4" onChange={handleInputChange} placeholder="Enter Hull Numbers" />
 
 
-            <button class="btn btn-info btn-block" type="submit">Save</button>
+            <button class="btn btn-info btn-block" onClick={(event) =>{
+                event.preventDefault();
+                handleNewVehicleSubmit();
+                }} type="submit">Save</button>
             </form>
         </div>
     )
